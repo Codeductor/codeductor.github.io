3513afed5a7db6c9ce04b1b0c5c11dce1f8b25e9
Brent Scarafone <brentscarafone@gmail.com>
2026-04-23 00:13:39 -0600

    style: switch to Coda font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Coda, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const coda = Coda({
+  variable: "--font-sans",
   subsets: ["latin"],
+  weight: ["400", "800"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  variable: "--font-mono",
   subsets: ["latin"],
 });
 
@@ -23,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${coda.variable} ${geistMono.variable}`}>
       <body>{children}</body>
     </html>
   );
